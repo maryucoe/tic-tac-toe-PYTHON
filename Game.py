be78223dbab174a5a8567bd9ca05dc68f76b78c6
Mary <119369807+maryucoe@users.noreply.github.com>
2024-07-25 05:10:50 +0400

    FINAL

--- Game.py
+++ Game.py
@@ -1,6 +1,76 @@
 import tkinter 
 def settile(row,column):
-    pass
+    global curr_player
+
+    if(game_over):
+        return  
+
+
+    if board[row][column]["text"] !="":
+        return
+    
+
+    board[row][column]["text"]=curr_player
+    if curr_player==playerO:
+        curr_player=playerx
+    else:
+        curr_player=playerO
+    label["text"]=curr_player+"'s turn"
+    check_winner()
+
+
+def  check_winner():
+    global turns,game_over
+    turns+=1
+
+    for row in range(3):
+        if(board[row][0]["text"]==board[row][1]["text"]==board[row][2]["text"]
+           and board[row][0]["text"]!=""):
+            label.config(text=board[row][0]["text"]+" is the winner!", foreground=color_yellow)
+            for column in range(3):
+                board[row][column].config(foreground=color_yellow,background="purple")
+            game_over=True
+            return
+    for column in range(3):
+        if(board[0][column]["text"]==board[1][column]["text"]==board[2][column]["text"]
+           and board[0][column]["text"]!=""):
+            label.config(text=board[0][column]["text"]+" is the winner!", foreground=color_yellow)
+            for row in range(3):
+                board[row][column].config(foreground=color_yellow,background="purple")
+            game_over=True
+            return
+        
+        if(board[0][0]["text"]==board[1][1]["text"]==board[2][2]["text"]
+           and board[0][0]["text"]!=""):
+            label.config(text=board[0][0]["text"]+" is the winner!", foreground=color_yellow)
+            for i in range(3):
+                board[i][i].config(foreground=color_yellow,background="purple")
+            game_over=True
+            return
+        
+
+        if(board[0][2]["text"]==board[1][1]["text"]==board[2][0]["text"]
+           and board[0][2]["text"]!=""):
+            label.config(text=board[0][2]["text"]+" is the winner!", foreground=color_yellow)
+            board[0][2].config(foreground=color_yellow,background="purple")
+            board[1][1].config(foreground=color_yellow,background="purple")
+            board[2][0].config(foreground=color_yellow,background="purple")
+            game_over=True
+            return
+        
+    if (turns==9):
+        game_over=True  
+        label.config(text="TIE!",foreground=color_yellow)  
+def new_game():
+    global turns,game_over
+    turns=0
+    game_over=False
+
+    label.config(text=curr_player+"'s turn",foreground="white")
+    for row in range(3):
+        for column in range(3):
+            board[row][column].config(text="",foreground="blue",background="gray")
+    
 
 playerx='X'
 playerO='O'
@@ -12,12 +82,16 @@ color_blue="blue"
 color_yellow="yellow"
 color_gray="gray"
 
+
+turns=0
+game_over=False
+
 window=tkinter.Tk()
 window.title("tic tac toe")
 window.resizable(False,False)
 
 frame=tkinter.Frame(window)
-label=tkinter.Label(frame,text=curr_player+"'s turn",foreground="black",font=("Consolas",20))
+label=tkinter.Label(frame,text=curr_player+"'s turn",foreground="white",background=color_gray,font=("Consolas",20))
 
 label.grid(row=0,column=0,columnspan=3,sticky="we")
 for row in range(3):
@@ -26,7 +100,21 @@ for row in range(3):
                                          background=color_gray,foreground=color_blue,width=4,height=1,
                                          command=lambda row=row,column=column:settile(row,column))
         board[row][column].grid(row=row+1,column=column)
+button=tkinter.Button(frame,text="Restart",font=("Consolas",20),background=color_gray,
+                      foreground="white",command=new_game)
+button.grid(row=4,column=0,columnspan=3,sticky="we")
+
 
 frame.pack()    
+window.update()
+window_width=window.winfo_width()
+window_height=window.winfo_height()
+screen_width=window.winfo_screenwidth()
+screen_height=window.winfo_screenheight()
+
+window_x=int((screen_width/2)-(window_width/2))
+window_y=int((screen_height/2)-(window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{window_x}+{window_y}")
 
 window.mainloop()
